bd1692fe67ba1a566b47b3fca6b008fb386674ba
jipro <wona23@gmail.com>
2018-08-29 15:41:55 +0900

    폼 인증검사 주석 처리

--- Instagram/WebContent/js/login.js
+++ Instagram/WebContent/js/login.js
@@ -1,37 +1,37 @@
 // Login
 $(document).ready(function() {
-    $('.ui.form').form({
-        inline : true,
-        on     : 'blur',
-        fields: {
-            email: {
-                identifier: 'email',
-                rules: [{
-                        type: 'empty',
-                        prompt: '이메일을 입력하세요.'
-                    },
-                    {
-                        type: 'email',
-                        prompt: '올바른 이메일 형식을 입력하세요.'
-                    }
-                ]
-            },
-            password: {
-                identifier: 'password',
-                rules: [{
-                        type: 'empty',
-                        prompt: '비밀번호를 입력하세요.'
-                    },
-                    {
-                        type: 'minLength[8]',
-                        prompt: '비밀번호는 8자 이상이어야 합니다.'
-                    },
-                    {
-                        type: 'regExp[^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).+$]',
-                        prompt: '비밀번호는 하나 이상의 영문 및 숫자, 특수 기호를 포함해야 합니다.'
-                    }
-                ]
-            }
-        }
-    });
+    // $('.ui.form').form({
+    //     inline : true,
+    //     on     : 'blur',
+    //     fields: {
+    //         email: {
+    //             identifier: 'email',
+    //             rules: [{
+    //                     type: 'empty',
+    //                     prompt: '이메일을 입력하세요.'
+    //                 },
+    //                 {
+    //                     type: 'email',
+    //                     prompt: '올바른 이메일 형식을 입력하세요.'
+    //                 }
+    //             ]
+    //         },
+    //         password: {
+    //             identifier: 'password',
+    //             rules: [{
+    //                     type: 'empty',
+    //                     prompt: '비밀번호를 입력하세요.'
+    //                 },
+    //                 {
+    //                     type: 'minLength[8]',
+    //                     prompt: '비밀번호는 8자 이상이어야 합니다.'
+    //                 },
+    //                 {
+    //                     type: 'regExp[^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).+$]',
+    //                     prompt: '비밀번호는 하나 이상의 영문 및 숫자, 특수 기호를 포함해야 합니다.'
+    //                 }
+    //             ]
+    //         }
+    //     }
+    // });
 });
