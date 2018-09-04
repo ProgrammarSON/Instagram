15b61a9009e7561a002d263641b0e82d1e896d50
JunWoo Son <sjw3957@gmail.com>
2018-09-04 17:30:54 +0900

    newsfeed 프로시저 추가

--- Instagram/WebContent/DB/Procedure.sql
+++ Instagram/WebContent/DB/Procedure.sql
@@ -15,6 +15,7 @@ BEGIN
   commit;
 END following_proc;
 
+
 create or replace PROCEDURE unfollow_proc(  
   puser_id IN myfeed.user_id%TYPE,
   pfollow_id IN myfeed.user_id%TYPE
@@ -30,3 +31,28 @@ BEGIN
   WHERE user_id = pfollow_id;
   commit;
 END unfollow_proc;
+
+
+create or replace PROCEDURE newsfeed_proc(  
+  puser_id IN NEWSFEED.USER_ID%TYPE,
+  pcontents IN NEWSFEED.CONTENTS%TYPE,
+  pimage_path IN NEWSFEED.IMAGE_PATH%TYPE,
+  pnewsfeed_id OUT NUMBER
+  ) IS
+  
+  feedID NUMBER;
+
+BEGIN
+  feedID := newsfeed_seq.nextval;
+  
+  INSERT INTO newsfeed
+  VALUES(feedID, puser_id, sysdate, pcontents, pimage_path);
+ 
+  pnewsfeed_id := feedID;  
+  commit;
+
+EXCEPTION 
+ WHEN OTHERS THEN
+    rollback;
+    pnewsfeed_id := -1;
+END newsfeed_proc;
