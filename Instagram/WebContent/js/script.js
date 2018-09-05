39c9228cd6b4c234f584779ee76f6671314197c5
jipro <wona23@gmail.com>
2018-09-05 11:45:29 +0900

    viewmyfeed 레이아웃 수정

--- Instagram/WebContent/js/script.js
+++ Instagram/WebContent/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
         $(this).addClass('active');
     });
 
-    $('#dimmer_pic.image').dimmer({
+    $('#dimmer_pic.image, .card .image').dimmer({
         on: 'hover'
     });
 });
