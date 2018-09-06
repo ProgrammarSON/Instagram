089d5021c94b16f7fb949736894d6e4b024869c2
jisang <wona23@gmail.com>
2018-09-06 22:42:23 +0900

    script 수정 중...

--- Instagram/WebContent/js/script.js
+++ Instagram/WebContent/js/script.js
@@ -10,11 +10,22 @@ $(document).ready(function() {
         on: 'hover'
     });
     
+	// 팔로우한 유저 모달창 띄우기 
+    $('#show_modal_follow').click(function(){
+        $('.ui.modal')
+        .modal({
+            closable: false,
+            transition: 'fade'
+        })
+        .modal('show');
+    });
+    
 /* ajax 통합 */
     $.ajax(
     // navbar 검색 기능
     	{
     		url : "searchid.do",
+    		type: "POST",
     		success : function(result){
     			var datas = JSON.parse(result);
     			var datajson = JSON.stringify(datas);
@@ -31,7 +42,7 @@ $(document).ready(function() {
     			$("#viewsearch").html(resulttag);
     		}
     	},
-    // 좋아요 feed 
+    // 좋아요 feed
 		{
 	        url: "likesfeed.html",
 	        type: "GET",
@@ -47,21 +58,30 @@ $(document).ready(function() {
 	        }
 	    }
     );
-	
-//    var like = $.post("likesfeed.html", function(data) {
-//    	console.log("success!");
-//    }).
-//    	done(function(data) {
-//            $("#liked").popup({
-//                popup: $(".custom.popup").html(data),
-//                on: 'click'
-//            });
-//        })
-//        .fail(function(err, val, msg) {
-//            alert(err.responseText);
-//        }
-//	);
 
+//    $.when($.ajax("likesfeed.html")).done(function(data) {
+//    	console.log("success!");
+//        $("#liked").popup({
+//            popup: $(".custom.popup").html(data),
+//            on: 'click'
+//        });
+//    }),
+//    ($.ajax("searchid.do")).done(function(result) {
+//		var datas = JSON.parse(result);
+//		var datajson = JSON.stringify(datas);
+//		console.log(datajson);
+//		var resulttag;
+//		console.log(datas);
+//		resulttag += "<table border='1'>";
+//		for(i=0; i<datas.length; i++){
+//			resulttag += "<tr><td><a href=viewmyfeed.do?user_id="+datas[i].user_id+">"
+//			+datas[i].user_id
+//			+"</a></td></tr>";
+//		}			
+//		resulttag += "</table>";
+//		$("#viewsearch").html(resulttag);
+//    });
+    
 	$("#searchid").on("keyup", function(){
 		var value = $(this).val().toLowerCase();
 		$("table tr").filter(function(){
@@ -79,3 +99,25 @@ function readURL(input){
         reader.readAsDataURL(input.files[0]);
     } 
 }
+
+//function ajaxSearch() {
+//	var deferred = $.Deferred();
+//	
+//	XMLHttpRequest xhr = new XMLHttpRequest();
+//	xhr.open("POST", "result", true);
+//	
+//	xhr.addEventListener('load', function(){
+//		if(xhr.status == 200) {
+//			deferred.resolve(xhr.response);
+//		} else {
+//			deferred.reject("HTTP error: " + xhr.status);
+//		}
+//	}, false)
+//	
+//	xhr.send();
+//	return deferred.promise();
+//};
+//
+//function ajaxLikeFeed() {
+//	
+//}
